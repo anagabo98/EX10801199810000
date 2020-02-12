aa623c6d33166d675e329653a0ea4e9658f71f63
Ana Gabriela Fiallos <anagabo98@gmail.com>
2020-02-11 19:31:32 -0600

    metodo post

--- routes/api/seguridad/seguridad.js
+++ routes/api/seguridad/seguridad.js
@@ -13,6 +13,12 @@ router.get('/users/:id',(req, res)=>{
     return res.status(200).json(user);
 });
 
+router.post('/users/new', (req, res)=>{
+    var datosEnviados = req.body;
+    var newUser = userModel.addNew(datosEnviados);
+    return res.status(200).json(newUser);
+  }); 
+  
 
 
 module.exports = router;
